f46f2629046a4d478a7c361db1fc773a867460d6
Mugisha-m <mugishaalbert999@gmail.com>
2026-02-14 22:12:22 +0200

    models added in

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,7 @@
 const message: string = "backend is running";
 console.log(message);
+
+import { Prisma } from '@prisma/client'
+import { config } from '../prisma/prisma.config'  // adjust path if needed
+
+const prisma = new Prisma.PrismaClient({ ...config })
