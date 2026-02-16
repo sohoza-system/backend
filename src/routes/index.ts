d9694b29ca6087db2b649de6736a1a41ad0cd98e
mugishastev <mugishas614@gmail.com>
2026-02-16 23:16:46 +0200

    Introduce Prisma schema with User, TeamMember, and Service models, and implement their respective CRUD APIs.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,14 @@ import express from 'express';
 import userRouter from './userRoute';
 import authRouter from './authRoute';
 
+import teamMemberRouter from './teamMemberRoute';
+import serviceRouter from './serviceRoute';
+
 const router = express.Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
+router.use('/team-members', teamMemberRouter);
+router.use('/services', serviceRouter);
 
 export default router;
